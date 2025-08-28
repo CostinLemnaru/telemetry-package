ae444bde4b5fd8b8335c90194df603f0aaba0d99
Costin Lemnaru <costinlemnaru@gmail.com>
2025-08-28 13:59:50 +0300

    tests for all Logger methods

--- tests/LoggerTest.php
+++ tests/LoggerTest.php
@@ -8,23 +8,68 @@ use Telemetry\Drivers\CliDriver;
 use Telemetry\Drivers\DriverInterface;
 
 class LoggerTest extends TestCase {
-    public function testCliDriver() 
+
+    public function testInfo()
     {
         $cliDriver = new CliDriver();
         $logger = new Logger([$cliDriver]);
-
         $tags = [
             'id' => 100320,
             'source' => 'api'
         ];
 
-        $this->expectOutputString("Testing CLI Driver.");
-
-
         $this->expectOutputRegex('/'.DriverInterface::INFO.': Testing CLI Driver/');
         $this->expectOutputRegex('/"id":100320/');
+        $this->expectOutputRegex('/"source":"api"/');
 
-        $logger->info("testing cli driver", $tags);
+        $logger->info("Testing CLI Driver", $tags);
+    }
 
+    public function testDebug()
+    {
+        $cliDriver = new CliDriver();
+        $logger = new Logger([$cliDriver]);
+        $tags = [
+            'id' => 100320,
+            'source' => 'api'
+        ];
+
+        $this->expectOutputRegex('/'.DriverInterface::DEBUG.': Debug message/');
+        $this->expectOutputRegex('/"id":100320/');
+        $this->expectOutputRegex('/"source":"api"/');
+
+        $logger->debug("Debug message", $tags);
+    }
+
+    public function testWarning()
+    {
+        $cliDriver = new CliDriver();
+        $logger = new Logger([$cliDriver]);
+        $tags = [
+            'id' => 100320,
+            'source' => 'api'
+        ];
+
+        $this->expectOutputRegex('/'.DriverInterface::WARNING.': Some warn logged/');
+        $this->expectOutputRegex('/"id":100320/');
+        $this->expectOutputRegex('/"source":"api"/');
+
+        $logger->warning("Some warn logged", $tags);
+    }
+
+    public function testError()
+    {
+        $cliDriver = new CliDriver();
+        $logger = new Logger([$cliDriver]);
+        $tags = [
+            'id' => 100320,
+            'source' => 'api'
+        ];
+
+        $this->expectOutputRegex('/'.DriverInterface::ERROR.': server error/');
+        $this->expectOutputRegex('/"id":100320/');
+        $this->expectOutputRegex('/"source":"api"/');
+
+        $logger->error("server error", $tags);
     }
 }
